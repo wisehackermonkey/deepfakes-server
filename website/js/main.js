a615d7a8884b4e814b08fe4f26f674029a2ed888
Oran C <Oranbusiness@gmail.com>
2020-04-07 15:43:39 -0700

    [add] started website

---
+++ website/js/main.js
@@ -0,0 +1,9 @@
+// sumit script for ml deepfake website
+// by oran collins
+// github.com/wisehackermonkey
+// oranbusiness@gmail.com
+// 20200407
+
+window.onload(e => {
+    console.log("loaded page!");
+})
